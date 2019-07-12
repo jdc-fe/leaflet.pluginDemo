3bebe9b17e9230baded6ea2dd852a43300a5c8a8
houlinjiang <houlinjiang@jd.com>
2019-07-12 15:57:13 +0800

    add points

--- src/index.js
+++ src/index.js
@@ -1,25 +1,45 @@
-
 L.MyLayer = L.Layer.extend({
-  // start|end = [lat, lng]
-  initialize({ start, end }, { color = 'red' }) {
-    this.start = start;
-    this.end = end;
+  // data [{lat, lng}], style: { colore }
+  initialize(positions, { color = 'red' }) {
+    this.positions = positions;
     this.color = color;
   },
   // layer api: https://leafletjs.com/reference-1.5.0.html#layer-onadd
   onAdd(map) {
     this._map = map;
-
-    const container = L.DomUtil.create('div', 'leaflet-MyLayer-container');
+    const { overlayPane } = this._map.getPanes();
+    const container = L.DomUtil.create(
+      'div', 'leaflet-MyLayer-container', overlayPane
+    );
     this.container = container;
     const { x, y } = this._map.getSize();
     Object.assign(container.style, {
       position: 'absolute',
       width: `${x}px`,
       height: `${y}px`,
-      background: 'red'
+      // background: 'rgba(255, 0, 0, .3)'
     });
-    this._map.getPane().overlayPane().appendChild(container);
+    this.initPoints();
+  },
+
+  initPoints() {
+    this.positions.forEach(({ lat, lng }) => {
+      const point = L.DomUtil.create('div', '', this.container);
+      const { x, y } = this._map.latLngToLayerPoint({ lat, lng });
+      Object.assign(point.style, {
+        position: 'absolute',
+        left: `${x}px`,
+        top: `${y}px`,
+        width: '10px',
+        height: '10px',
+        borderRadius: '50%',
+        background: 'red'
+      });
+    });
+  },
+
+  onRemove() {
+    L.DomUtil.remove(this.container);
   }
 });
 
